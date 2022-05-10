8ca1af9b3b5e8fa7eb4f0007858780342c668783
xlong7 <64448589+xlong7@users.noreply.github.com>
2022-05-09 20:40:44 -0400

    Update Header.js

--- example/frontend/components/Header.js
+++ example/frontend/components/Header.js
@@ -1,40 +1,70 @@
 import React from "react";
 import Link from "next/link";
-import { Menu, Container, Card, Grid } from "semantic-ui-react";
+import { Menu, Container, Card, Grid,Image} from "semantic-ui-react";
 
 
-const time = "00:10:00";
+const time = "00:05:00";
 const items = [
-  {header: '102934',
+  {header: '90012',
   description: "ETH"}
+]
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'olive',
+  'green',
+  'teal',
+  'blue',
+  'violet',
+  'purple',
+  'pink',
+  'brown',
+  'grey',
+  'black',
 ]
 export default () => {
   return (
     <Container>
-    <Menu style={{ marginTop: "10px" }}>
       <Link href="/">
-        <img src="/mag_logo_2.jpeg" alt="Logo"/>
+        <Image src="/cover1.jpg" fluid/>
       </Link>
-
-      <Menu.Menu position="right">
+      
+    <Menu color={colors[0]} widths={3}>
+      <Menu.Item name='home'>
+        
+      </Menu.Item>
+      <Menu.Item name='About'>
+        
+      </Menu.Item>
+      <Menu.Item>
         <Link href="/">
           <a className="item">Contribute Now!</a>
         </Link>
-      </Menu.Menu>
-    </Menu>
+      </Menu.Item>
 
-    <Card centered color="orange">
+   
+    </Menu>
+    <Card.Group>
+    <Card centered color="red">
+    <Image src="/pic1.jpeg" wrapped ui={false}/>
+
       <Card.Content>
-        <Card.Header> 120384 ETH</Card.Header>
-        <Card.Description>  waiting to be distributed in MagPool! </Card.Description>
+        <Card.Header> 90000 ETH</Card.Header>
+        <Card.Description>  waiting for distribution </Card.Description>
       </Card.Content>
-      <Card.Content extra>
+      
+    </Card>
+    <Card centered color="blue">
+    <Image src="/pic2.jpeg" wrapped ui={false}/>
+    <Card.Content>
         <Grid columns={2}>
           <Grid.Column key="1" width={5}> <h3> {time} </h3></Grid.Column>
           <Grid.Column key="2" width = {11}> <p> hours till next distribution </p></Grid.Column>
        </Grid>
       </Card.Content>
-    </Card>
+      </Card>
+      </Card.Group>
     </Container>
   );
 };
