6a57abba23e7c6e0c4b140d9db2d8c39fab6f044
xlong7 <64448589+xlong7@users.noreply.github.com>
2022-05-09 20:41:34 -0400

    Update Categories.js

--- example/frontend/components/Categories.js
+++ example/frontend/components/Categories.js
@@ -1,10 +1,13 @@
 export const countryList = [
   { key: 'af', value: 'Afghanistan', flag: 'af', text: 'Afghanistan' },
-{ key: 'ax', value: 'Aland Islands', flag: 'ax', text: 'Aland Islands' },
-{ key: 'al', value: 'Albania', flag: 'al', text: 'Albania' },
-{ key: 'dz', value: 'Algeria', flag: 'dz', text: 'Algeria' },
-{ key: 'as', value: 'American Samoa', flag: 'as', text: 'American Samoa' },
-{ key: 'ad', value: 'Andorra', flag: 'ad', text: 'Andorra' },
+{ key: 'ax', value: 'Albania', flag: 'ax', text: 'Albania' },
+{ key: 'al', value: 'Bahamas', flag: 'al', text: '	Bahamas' },
+{ key: 'dz', value: 'Bangladesh', flag: 'dz', text: 'Bangladesh' },
+{ key: 'as', value: 'Botswana', flag: 'as', text: 'Botswana' },
+{ key: 'ad', value: 'Brazil', flag: 'ad', text: 'Brazil' },
+{ key: 'ae', value: 'Malaysia', flag: 'ae', text: 'Malaysia' },
+{ key: 'ag', value: 'Mexico', flag: 'ag', text: '	Mexico' },
+{ key: 'ai', value: 'Nepal', flag: 'ai', text: 'Nepal' },
 ];
 
 export default countryList;
